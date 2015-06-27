7c6c26dc054958619907a0a4992992a9fe018416
Jethro <stanza@Smyrna.local>
2015-06-26 23:42:37 -0700

    Added models and code to spread fire

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -9,8 +9,11 @@ public class Map : MonoBehaviour {
 	public Transform burnedThing;
 	public Transform house;
 	public Transform park;
+	
+	public Material burnedMat;
+	public Material fireMat;
 
-	static int maxSize = 10;
+	static int maxSize = 100;
 	public int[,] 			map 		= new int[maxSize, maxSize];
 	public Transform[,] 	mapObjects	= new Transform[maxSize, maxSize];
 	// 0 = street
@@ -24,7 +27,7 @@ public class Map : MonoBehaviour {
 	
 		for(int i = 0; i < maxSize; i++) {
 			for(int j = 0; j < maxSize; j++) {
-				if((i % 3) == 0 || (j % 3) == 0 || Random.Range(0f, 1f) > 0.9f) {
+				if(((i % 3) == 0 || (j % 3) == 0) && Random.Range(0f, 1f) < 0.9f) {
 					map[i, j] = 0;
 					mapObjects[i,j] = Instantiate(street);
 				} else {
@@ -36,7 +39,11 @@ public class Map : MonoBehaviour {
 						mapObjects[i, j] = Instantiate(park);
 					}
 				}
-				mapObjects[i, j].position = new Vector3(i, 0, j);
+				mapObjects[i, j].SetParent(this.transform);
+				mapObjects[i, j].position = new Vector3(i*100, 0, j*100);
+				mapObjects[i, j].gameObject.AddComponent<SetFire>();
+				SetFire s = mapObjects[i,j].GetComponent<SetFire>();
+				s.x = i; s.y = j;
 			}
 		}
 		
@@ -45,5 +52,19 @@ public class Map : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	public void SetThingsOnFire(int x, int y) {
+		if(x >= maxSize || y >= maxSize || x < 0 || y < 0) {
+			return;
+		}
+		SetFire s = mapObjects[x,y].GetComponent<SetFire>() as SetFire;
+		s.setFire();
+	}
+	public Material getBurnedMat() {
+		return burnedMat;
+	}
+	public Material getFireMat() {
+		return fireMat;
 	}
 }
