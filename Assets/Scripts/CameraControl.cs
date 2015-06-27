3e2a72b9c67d0d4dca157a46245a0fc8259b8c85
unknown <jta.allard@gmail.com>
2015-06-26 23:47:36 -0700

    3d cursor

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
+	public GameObject cursor;
 	float movementSpeed = 0.05f;
 	float borderMargin = 50;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,20 +16,31 @@ public class CameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Move right
-		if ( Input.mousePosition.x > Screen.width - borderMargin ) {
+		if (Input.mousePosition.x > Screen.width - borderMargin) {
 			transform.Translate( movementSpeed, 0, 0, Space.World );
 		}
 		//Move left
-		if ( Input.mousePosition.x < borderMargin ) {
+		if (Input.mousePosition.x < borderMargin) {
 			transform.Translate( -movementSpeed, 0, 0, Space.World );
 		}
 		//Move up
-		if ( Input.mousePosition.y > Screen.height - borderMargin ) {
+		if (Input.mousePosition.y > Screen.height - borderMargin) {
 			transform.Translate( 0, 0, movementSpeed, Space.World );
 		}
 		//Move down
-		if ( Input.mousePosition.y < borderMargin ) {
+		if (Input.mousePosition.y < borderMargin) {
 			transform.Translate( 0, 0, -movementSpeed, Space.World );
 		}
+
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast (ray, out hit)) {
+			cursor.SetActive(true);
+			cursor.transform.position = hit.point;
+		} else {
+			cursor.SetActive(false);
+		}
+
 	}
 }
