5fab6d2bb20c588a36aba536045d988f21e38b5c
unknown <jta.allard@gmail.com>
2015-06-26 22:27:58 -0700

    Added camera control

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	float movementSpeed = 0.05f;
+	float borderMargin = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Move right
+		if ( Input.mousePosition.x > Screen.width - borderMargin ) {
+			transform.Translate( movementSpeed, 0, 0, Space.World );
+		}
+		//Move left
+		if ( Input.mousePosition.x < borderMargin ) {
+			transform.Translate( -movementSpeed, 0, 0, Space.World );
+		}
+		//Move up
+		if ( Input.mousePosition.y > Screen.height - borderMargin ) {
+			transform.Translate( 0, 0, movementSpeed, Space.World );
+		}
+		//Move down
+		if ( Input.mousePosition.y < borderMargin ) {
+			transform.Translate( 0, 0, -movementSpeed, Space.World );
+		}
+	}
+}
