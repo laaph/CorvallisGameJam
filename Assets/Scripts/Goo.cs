7bc92714a5157802633cf149af2a2f3e450b6bed
Macklehatton <macklehatton@gmail.com>
2015-06-27 21:57:06 -0700

    added goo scripts

---
+++ Assets/Scripts/Goo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goo : MonoBehaviour {
+
+    public int ProgressionNumber { get; set; }
+
+    void Awake()
+    {
+        ProgressionNumber = 0;
+    }
+}
